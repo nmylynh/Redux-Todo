8b80c1a828e3946abb81fc79dcaaa82a3468c676
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2019-05-15 02:07:32 -0700

    added a form for adding todos

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import TodosList from './components/TodosList';
+import AddTodo from './components/AddTodo';
 import './App.css';
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div>
       <TodosList />
+      <AddTodo />
       </div>
     );
   }
