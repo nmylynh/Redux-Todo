ba1626a132dedb22eeabd378aa63a30feba07395
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2019-05-15 00:44:44 -0700

    added reducer that manages state updates
    reducers describe state mutations by taking in previous state, dispatching an action, and returning new state.

---
+++ todo/src/components/TodosList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class TodosList extends Component {
+  render() {
+    return (
+      <div>
+        placeholder
+      </div>
+    )
+  }
+}
+
+export default TodosList
--- todo/src/index.js
+++ todo/src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import reducers from './reducers';
+import todos from './reducers';
 
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
-    <Provider store={createStore(reducers)}>
+    <Provider store={createStore(todos)}>
       <App />
     </Provider>
     , document.getElementById('root')
