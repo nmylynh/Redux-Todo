4c20dec0d03b0d667295f922b6b77cc929d7def9
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2019-05-16 20:40:41 -0700

    set initial state for reducer

--- todo/src/index.js
+++ todo/src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
-import './index.css';
 import App from './App';
+import rootReducer from './reducers';
 
-const store = createStore(todos);
+const store = createStore(rootReducer);
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
